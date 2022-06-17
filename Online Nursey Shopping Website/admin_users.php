b608c001eb4d4ac507bfca9c81c1e39ee4425440
Suleyman Anwar <63181973+suleman24@users.noreply.github.com>
2022-06-17 22:46:36 +0500

    admin accounts page and functionality done

---
+++ Online Nursey Shopping Website/admin_users.php
@@ -0,0 +1,116 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:login.php');
+}
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   mysqli_query($conn, "DELETE FROM `users` WHERE id = '$delete_id'") or die('query failed');
+   header('location:admin_users.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>users</title>
+
+	<link rel="stylesheet" href="css/header.css">
+   <link rel="stylesheet" href="css/admin_users.css">
+
+</head>
+<body>
+   
+<?php include 'admin_header.php'; ?>
+
+
+<section class="accounts">
+<center>    <h1 class="title">Accounts</h1>  </center>
+
+
+   <div class="box-container">
+   
+
+<div class="box">
+         <?php 
+            $select_account = mysqli_query($conn, "SELECT * FROM `users`") or die('query failed');
+            $number_of_account = mysqli_num_rows($select_account);
+         ?>
+		 <p>Total Accounts</p>
+         <h3><?php echo $number_of_account; ?></h3>
+         
+      </div>
+	  
+	  
+	  
+	  
+      <div class="box">
+         <?php 
+            $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'user'") or die('query failed');
+            $number_of_users = mysqli_num_rows($select_users);
+         ?>
+		 <p>User Accounts</p>
+         <h3><?php echo $number_of_users; ?></h3>
+         
+      </div>
+
+      <div class="box">
+         <?php 
+            $select_admins = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'admin'") or die('query failed');
+            $number_of_admins = mysqli_num_rows($select_admins);
+         ?>
+		 <p>Admin Accounts</p>
+         <h3><?php echo $number_of_admins; ?></h3>
+         
+      </div>
+	   </div>
+	   
+	    </section>
+	  
+	  
+<section class="users">
+
+
+   <div class="box-container">
+      <?php
+         $select_users = mysqli_query($conn, "SELECT * FROM `users`") or die('query failed');
+         while($fetch_users = mysqli_fetch_assoc($select_users)){
+      ?>
+      <div class="box">
+         <p> user id : <span><?php echo $fetch_users['id']; ?></span> </p>
+         <p> username : <span><?php echo $fetch_users['name']; ?></span> </p>
+         <p> email : <span><?php echo $fetch_users['email']; ?></span> </p>
+         <p> user type : <span style="color:<?php if($fetch_users['user_type'] == 'admin'){ echo 'red'; } ?>"><?php echo $fetch_users['user_type']; ?></span> </p>
+         <a href="admin_users.php?delete=<?php echo $fetch_users['id']; ?>" onclick="return confirm('delete this user?');" class="delete-btn">delete user</a>
+      </div>
+      <?php
+         };
+      ?>
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<!-- custom admin js file link  -->
+<script src="js/admin_script.js"></script>
+
+</body>
+</html>
